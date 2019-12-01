ca1b8f769e9f921e9087538c93ab5f4389d73bd2
Felix Plymouth <fplymouth@gmail.com>
2019-12-01 12:16:47 +0100

    Day 1 Complete

---
+++ ConsoleApplication1/Day1.h
@@ -0,0 +1,26 @@
+#pragma 
+
+#include <array>;
+
+class Day1
+{
+private:
+	static int GetFuleReq(int mass);
+	static int GetItterativeFuelReq(int mass);
+public:	
+	template<std::size_t N>
+	static int GetSumOfFuelReqs(std::array<int, N> masses);
+	static int GetSumOfFuelReqs();
+	static int GetItterativeSumOfFuelReqs();
+
+
+};
+
+template<std::size_t N> 
+int Day1::GetSumOfFuelReqs(std::array<int, N> masses) {
+	int n = 0;
+	for (int i = 0; i < N; i++) {
+		n += Day1::GetFuleReq(masses[i]);
+	}
+	return n;
+}
